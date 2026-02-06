fd7755447b7d3d1ed0c0adfe4080dab5e48f6ca5
Toni <toni@example.com>
2026-02-06 17:53:39 +0100

    Cambiar imagen de Junior a logo-pato-nuevo para mejor visualizacion

--- src/jepgc/components/QuizGame.tsx
+++ src/jepgc/components/QuizGame.tsx
@@ -72,7 +72,7 @@ const QuizGame: React.FC<QuizGameProps> = ({ onFinish, onCancel }) => {
                 <div className="level-grid">
                     <button className="level-card junior" onClick={() => startLevel(Level.JUNIOR)}>
                         <span className="level-icon">
-                            <img src="/img/img-contable.webp" alt="Junior" className="level-img" />
+                            <img src="/img/logo-pato-nuevo.webp" alt="Junior" className="level-img" />
                         </span>
                         <span className="level-name">Junior</span>
                         <span className="level-desc">{t('game.difficulty.easy')}</span>
